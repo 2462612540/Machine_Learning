49a5284383893df361504a4c7b4c851d1a5fcda2
xjl <18279148786@163.com>
2021-03-04 14:49:50 +0800

    Python语言练习的示范代码

---
+++ Python_Base/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@version: 1.0
+@author: xjl
+@file: __init__.py.py
+@time: 2021/3/4 14:31
+这是有关于的python的语言的基础的练习的代码
+"""
+if __name__ == '__main__':
+    pass
---
+++ Python_Base/python_source/Chapter21/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@version: 1.0
+@author: xjl
+@file: __init__.py.py
+@time: 2021/3/4 14:31
+"""
+if __name__ == '__main__':
+    pass
